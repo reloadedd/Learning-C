a1ea975f7e209b9bc7703b8d976372724dbad0a8
R E L O A D E D D <reloadedd@protonmail.com>
2020-03-14 22:30:52 +0200

    Add solution for assignment 11

---
+++ Assignment-11/solution.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int SIZE = 1;
+
+int main(int argc, char const *argv[])
+{
+	int *array = malloc(SIZE), sum;
+	float average;
+	char answer[] = "Y";
+
+	while ((strcmp(answer, "Y") == 0)) { 
+		printf("Enter a test score >>> ");
+		scanf("%d", &array[SIZE - 1]);
+
+		printf("Would you like to continue? Y/N >>> ");
+		scanf("%s", answer);
+
+		if (strcmp(answer, "Y") == 0) {
+			SIZE++;
+			array = realloc(array, SIZE);
+		}
+	}
+
+	sum = 0;
+	for (int i = 0; i < SIZE; ++i) {
+		sum += array[i];
+	}
+
+	average = sum / (SIZE * 1.0);
+	printf("%.2f is the average\n", average);
+
+	return 0;
+}
